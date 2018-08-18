eb5fd3326468baf982ae75e489d855844e65f721
Jason Heard <jasonpheard@gmail.com>
2018-08-18 10:13:59 -0600

    Switch to BPM and offset for beats

--- func/music.lua
+++ func/music.lua
@@ -11,94 +11,25 @@ function getSongData()
 	return songDataShared
 end
 
-function getMax(table)
-	local max = table[1]
-	for i = 2, #table do
-		if table[i] > max then
-			max = table[i]
-		end
-	end
 
-	return max
-end
-
-function getMin(table)
-	local min = table[1]
-	for i = 2, #table do
-		if table[i] < min then
-			min = table[i]
-		end
-	end
-
-	return min
-end
-
--- the frequency used for the low pass filter
-local lowPassFreq = 140
--- the beat cutoff amount
-local beatCutoff = 100
--- the goal Hz of the analysis
-local reduction = 1000
+local beatsPerMinute = 132
+local offset = 0.2
 
 function getBeats()
 	local songData = getSongData()
-	print("Duration is " .. songData:getDuration())
-	local sampleRate = songData:getSampleRate()
-	print("Sample rate is " .. sampleRate)
-	local sampleCount = songData:getSampleCount()
-	print("Sample count is " .. sampleCount)
-	local samples = {}
+	local duration = songData:getDuration()
+	print("Duration is " .. duration)
 
-	for i = 1, sampleCount do
-		samples[i] = songData:getSample(i - 1)
-	end
+	local secondsPerBeat = 60 / beatsPerMinute
+	print("Seconds per beat: " .. secondsPerBeat)
+	local nextBeat = offset
 
-	-- tau is the number of seconds in one wavelength of the low pass frequency
-	local tau = 1 / lowPassFreq
-	-- formula for alpha from https://www.embeddedrelated.com/showarticle/779.php
-	local alpha = (1 / sampleRate) / tau
-
-	-- perform low pass filtering
-	local lowPasSamples = {}
-	local yk = 0
-	for i = 1, sampleCount do
-		yk = yk + alpha * (samples[i] - yk)
-		lowPasSamples[i] = yk
-	end
-
-	-- average samples in groups of "reduction"
-	local reducedSamples = {}
-	local reducedSamplesCount = sampleCount / reduction
-	for i = 1, reducedSamplesCount do
-		-- local total = 0
-		-- for j = 1, reduction do
-		-- 	local sample = (i - 1) * reduction + j
-		-- 	total = total + math.abs(lowPasSamples[sample])
-		-- end
-		-- reducedSamples[i] = total / reduction
-
-		reducedSamples[i] = lowPasSamples[(i - 1) * reduction + 1]
-	end
-
-	-- average samples in groups of "reduction"
-	local normalizedSamples = {}
-	normalizedSamples[1] = math.abs(reducedSamples[1])
-	for i = 2, reducedSamplesCount do
-		normalizedSamples[i] = math.abs(reducedSamples[i] - reducedSamples[i - 1])
-	end
-
-	-- find min and max to generate a beat threshold
-	local max = getMax(normalizedSamples)
-	local min = getMin(normalizedSamples)
-	local threshold = ((max - min) * 0.3) + min
-
-	-- find the beats
 	local beats = {}
-	for i = 1, reducedSamplesCount - 2 do
-		if normalizedSamples[i] > threshold and normalizedSamples[i + 1] < threshold and normalizedSamples[i + 2] < threshold then
-			table.insert(beats, (i * reduction / sampleRate))
-			print("Beat at " .. (i * reduction / sampleRate))
-		end
+	while nextBeat < duration do
+		table.insert(beats, nextBeat)
+		print("Beat at " .. nextBeat)
+
+		nextBeat = nextBeat + secondsPerBeat
 	end
 
 	local source = love.audio.newSource(songData)
