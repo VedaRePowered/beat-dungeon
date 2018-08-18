5ce0b50dd0342eb4745d8f40b98343729a9c6dc2
Jason Heard <jasonpheard@gmail.com>
2018-08-18 13:26:42 -0600

    Allow user to move with the keyboard

--- func/player.lua
+++ func/player.lua
@@ -6,21 +6,29 @@ local playerHeight = 32
 local joystick
 function player.initializeJoystick()
 	joystick = love.joystick.getJoysticks()[1]
-
-	if joystick == nil then
-		print("Please plug in a joystick before starting the game.")
-		love.event.quit(-1)
-	end
 end
 function player.getPosition()
 	return playerX, playerY
 end
 function player.update(delta)
-	if math.abs(joystick:getAxis(1)) > 0.1 then
-		playerX = playerX + joystick:getAxis(1) * delta * 4
-	end
-	if math.abs(joystick:getAxis(2)) > 0.1 then
-		playerY = playerY - joystick:getAxis(2) * delta * 4
+	if joystick then
+		if math.abs(joystick:getAxis(1)) > 0.1 then
+			playerX = playerX + joystick:getAxis(1) * delta * 4
+		end
+		if math.abs(joystick:getAxis(2)) > 0.1 then
+			playerY = playerY - joystick:getAxis(2) * delta * 4
+		end
+	else
+		if love.keyboard.isDown("up") then
+			playerY = playerY + delta * 4
+		elseif love.keyboard.isDown("down") then
+			playerY = playerY - delta * 4
+		end
+		if love.keyboard.isDown("right") then
+			playerX = playerX + delta * 4
+		elseif love.keyboard.isDown("left") then
+			playerX = playerX - delta * 4
+		end
 	end
 
 	local width, height = world.getSize()
