c5f658978677d27d0079cd5972c8c1aca61c15b8
Jason Heard <jasonpheard@gmail.com>
2018-08-19 15:10:52 -0600

    Refactor menu to prepare for joystick

--- func/menu.lua
+++ func/menu.lua
@@ -7,6 +7,24 @@ local selectedSong = "Bensound - House.mp3"
 local difficulty = 1
 menuSong.play()
 
+function drawMenuOption(row, column, text, mouseX, mouseY)
+	local mouseColumn = math.floor(mouseX / 640) + 1
+	local baseX = (column - 1) * 640
+	local baseY = -36 + row * 36
+	if mouseColumn == column then
+		love.graphics.print(text, math.max(baseX + 18 - math.abs(mouseY - baseY - 20), baseX), baseY)
+	else
+		love.graphics.print(text, baseX, baseY)
+	end
+end
+
+function getSelectedMenuOption(mouseX, mouseY)
+	local mouseColumn = math.floor(mouseX / 640) + 1
+	local mouseRow = math.floor((mouseY + 36) / 36)
+
+	return mouseRow, mouseColumn
+end
+
 function menu.update(delta)
 	beatsPassed, hasEnded = menuSong.getBeatsPassed(delta)
 	if hasEnded then
@@ -18,39 +36,35 @@ function menu.update(delta)
 	if love.mouse.isDown(1) then
 		local mouseX, mouseY = love.mouse.getPosition()
 		if selectSong then
-			if mouseY > 124 and mouseY < 150 then
+			row, column = getSelectedMenuOption(mouseX, mouseY)
+			if row == 5 and column == 1 then
 				selectSong = false
-			end
-			local songs = love.filesystem.getDirectoryItems("assets/music")
-			for i, songName in ipairs(songs) do
-				local y = i
-				local x = 0
-				if y > 14 then
-					y = y - 14 - 6
-					x = 640
-				end
-				if mouseY > 168+y*36 and mouseY < 204+y*36 and mouseX > x and mouseX < x+640 then
-					selectedSong = songName
+			else
+				local songs = love.filesystem.getDirectoryItems("assets/music")
+				local index = (row - 6) + (column - 1) * 20
+				if songs[index] then
+					selectedSong = songs[index]
 				end
 			end
 		else
-			if mouseY > 150 and mouseY < 183 then
+			row, column = getSelectedMenuOption(mouseX, mouseY)
+			if row == 6 and column == 1 then
 				menuSong.stop()
 				song = music.loadSong("assets/music/" .. selectedSong, difficulty)
 				world.gen(16, song.getSongLength() * 4 + 32)
 				song.play()
 				mode = "game"
-			elseif mouseY > 183 and mouseY < 222 then
+			elseif row == 7 and column == 1 then
 				selectSong = true
-			elseif mouseY > 222 and mouseY < 258 then
+			elseif row == 8 and column == 1 then
 				love.event.quit(0)
-			elseif mouseY > 280 and mouseY < 316 then
+			elseif row == 10 and column == 1 then
 				difficulty = 0.5
 				menuSong.setBpmMultiplier(difficulty)
-			elseif mouseY > 316 and mouseY < 352 then
+			elseif row == 11 and column == 1 then
 				difficulty = 1
 				menuSong.setBpmMultiplier(difficulty)
-			elseif mouseY > 352 and mouseY < 388 then
+			elseif row == 12 and column == 1 then
 				difficulty = 2
 				menuSong.setBpmMultiplier(difficulty)
 			end
@@ -67,29 +81,25 @@ function menu.draw()
 	love.graphics.setColor((86 + ((225 - 86) * brightness)) / 256, (152 + ((242 - 152) * brightness)) / 256, (113 + ((235 - 113) * brightness)) / 256)
 	if selectSong then
 		local songs = love.filesystem.getDirectoryItems("assets/music")
-		love.graphics.print("Done", math.max(18-math.abs(mouseY-(132)), 0), 124)
+		drawMenuOption(5, 1, "Done", mouseX, mouseY)
 		for i, songName in ipairs(songs) do
-			local y = i
-			local x = 0
-			if y > 14 then
-				y = y - 14 - 6
-				x = 640
-			end
-			local xOffset = 0
-			if mouseX > x and mouseX < x+640 then
-				xOffset = 18-math.abs(mouseY-(186+y*36))
+			column = 1
+			row = 6 + i
+			if i > 14 then
+				column = 2
+				row = row - 20
 			end
 			songName = string.sub(songName, 1, string.len(songName)-4)
 			if songName == string.sub(selectedSong, 1, string.len(selectedSong)-4) then
 				songName = songName .. "*"
 			end
-			love.graphics.print(songName, math.max(xOffset+x, x), 168+y*36)
+			drawMenuOption(row, column, songName, mouseX, mouseY)
 		end
 	else
 		love.graphics.setFont(menuFont)
-		love.graphics.print("Play", math.max(18-math.abs(mouseY-168), 0), 150)
-		love.graphics.print("Select Song (" .. string.sub(selectedSong, 1, string.len(selectedSong)-4) .. ")", math.max(18-math.abs(mouseY-204), 0), 186)
-		love.graphics.print("Exit", math.max(18-math.abs(mouseY-240), 0), 222)
+		drawMenuOption(6, 1, "Play", mouseX, mouseY)
+		drawMenuOption(7, 1, "Select Song (" .. string.sub(selectedSong, 1, string.len(selectedSong)-4) .. ")", mouseX, mouseY)
+		drawMenuOption(8, 1, "Exit", mouseX, mouseY)
 
 		local de, dn, dh = "", "", ""
 		if difficulty == 0.5 then
@@ -99,9 +109,9 @@ function menu.draw()
 		elseif difficulty == 2 then
 			dh = "*"
 		end
-		love.graphics.print("Easy" .. de, math.max(18-math.abs(mouseY-298), 0), 280)
-		love.graphics.print("Normal" .. dn, math.max(18-math.abs(mouseY-334), 0), 316)
-		love.graphics.print("Hard" .. dh, math.max(18-math.abs(mouseY-370), 0), 352)
+		drawMenuOption(10, 1, "Easy" .. de, mouseX, mouseY)
+		drawMenuOption(11, 1, "Normal" .. dn, mouseX, mouseY)
+		drawMenuOption(12, 1, "Hard" .. dh, mouseX, mouseY)
 	end
 end
 
