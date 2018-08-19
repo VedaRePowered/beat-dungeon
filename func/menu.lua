4da51b22ac77aa4c98a4739d710b3a8480e904fe
BEN1JEN <benpheard@gmail.com>
2018-08-18 22:31:00 -0600

    Fix Menu

--- func/menu.lua
+++ func/menu.lua
@@ -1,9 +1,9 @@
 local menu = {}
 local logo = love.graphics.newImage("assets/logo.png")
-local menuSong = music.loadSong("assets/music/bensound-summer.mp3")
+local menuSong = music.loadSong("assets/music/Bensound - Summer.mp3")
 local menuFont = love.graphics.newFont("assets/fonts/pixelated.ttf", 36)
 local selectSong = false
-local selectedSong = "bensound-house.mp3"
+local selectedSong = "Bensound - House.mp3"
 menuSong.play()
 
 function menu.update(delta)
@@ -12,14 +12,20 @@ function menu.update(delta)
 		menuSong.play()
 	end
 	if love.mouse.isDown(1) then
-		local mouseY = love.mouse.getY()
+		local mouseX, mouseY = love.mouse.getPosition()
 		if selectSong then
-			if mouseY > 132 and mouseY < 168 then
+			if mouseY > 124 and mouseY < 150 then
 				selectSong = false
 			end
 			local songs = love.filesystem.getDirectoryItems("assets/music")
 			for i, songName in ipairs(songs) do
-				if mouseY > 150+i*36 and mouseY < 186+i*36 then
+				local y = i
+				local x = 0
+				if y > 14 then
+					y = y - 14 - 6
+					x = 640
+				end
+				if mouseY > 168+y*36 and mouseY < 204+y*36 and mouseX > x and mouseX < x+640 then
 					selectedSong = songName
 				end
 			end
@@ -40,30 +46,38 @@ function menu.update(delta)
 end
 
 function menu.draw()
-	local mouseY = love.mouse.getY()
+	local mouseX, mouseY = love.mouse.getPosition()
 	love.graphics.setColor(1, 1, 1)
 	love.graphics.draw(logo, 0, 0)
 	if beatsPassed > 0 then
-		love.graphics.setColor(225/256, 242/256, 235/256)
+		love.graphics.setColor(87890625, 0.9453125, 0.91796875)
 	else
-		love.graphics.setColor(86/256, 152/256, 113/256)
+		love.graphics.setColor(0.3359375, 0.59375, 0.44140625)
 	end
 	if selectSong then
 		local songs = love.filesystem.getDirectoryItems("assets/music")
-		love.graphics.print("Done", math.max(18-math.abs(mouseY-(150)), 0), 132)
+		love.graphics.print("Done", math.max(18-math.abs(mouseY-(132)), 0), 124)
 		for i, songName in ipairs(songs) do
 			local y = i
 			local x = 0
-			if y > 720 then
-				y = y - 720
+			if y > 14 then
+				y = y - 14 - 6
 				x = 640
 			end
-			love.graphics.print(string.sub(songName, 1, string.len(songName)-4), math.max(18-math.abs(mouseY-(186+y*36))+x, x), 168+y*36)
+			local xOffset = 0
+			if mouseX > x and mouseX < x+640 then
+				xOffset = 18-math.abs(mouseY-(186+y*36))
+			end
+			songName = string.sub(songName, 1, string.len(songName)-4)
+			if songName == string.sub(selectedSong, 1, string.len(selectedSong)-4) then
+				songName = songName .. "*"
+			end
+			love.graphics.print(songName, math.max(xOffset+x, x), 168+y*36)
 		end
 	else
 		love.graphics.setFont(menuFont)
 		love.graphics.print("Play", math.max(18-math.abs(mouseY-168), 0), 150)
-		love.graphics.print("Select Song", math.max(18-math.abs(mouseY-204), 0), 186)
+		love.graphics.print("Select Song (" .. string.sub(selectedSong, 1, string.len(selectedSong)-4) .. ")", math.max(18-math.abs(mouseY-204), 0), 186)
 		love.graphics.print("Exit", math.max(18-math.abs(mouseY-240), 0), 222)
 	end
 end
